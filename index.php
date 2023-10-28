7bffc1236a24c8606226e1b9a7cee3ee82fae9e0
randhirsingh23 <149237337+randhirsingh23@users.noreply.github.com>
2023-10-29 00:49:55 +0800

    latest10 index.php

--- index.php
+++ index.php
@@ -17,7 +17,10 @@
 
 		// Create database connection
 		$conn = new mysqli($servername, $username, $password, $dbname);
-			echo "after conn";
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 
 		// Check connection
 		if ($conn->connect_error) {
