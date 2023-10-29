d302a69732b6ca55424311248a01d98bdc21706b
randhirsingh23 <149237337+randhirsingh23@users.noreply.github.com>
2023-10-29 17:16:32 +0800

    New15 index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
-
+$conn = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, 'empdbserver2023.mysql.database.azure.com', 'dbadmin@empdbserver2023', 'P@ssword1234', 'employees_db', 3306, MYSQLI_CLIENT_SSL);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Connected successfully";
+if (mysqli_connect_errno()) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 $conn->close();
 ?>
+
+
